c2cb4ed4a931efcda014baeebd12f7f74a5e5f83
LycurgusxLycurgus <Extermporeeducacion@gmail.com>
2024-08-20 08:08:48 -0500

    cortosexelapp_0.1.1

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Get the port from the environment variable or default to 3000
+  const port = process.env.PORT || 3000; 
+
+  //  Update the CORS origin
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://your-frontend-domain.vercel.app', // Replace with your Vercel domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
-  await app.listen(3000);
+
+  await app.listen(port, '0.0.0.0'); // Explicitly listen on all interfaces (0.0.0.0)
+
+  console.log(`Application is running on: ${await app.getUrl()}`); 
 }
 bootstrap();
